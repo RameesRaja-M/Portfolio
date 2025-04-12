1fcd9d6f6ada127942ff9caf7413e145dcaf1b3b
RameesRaja-M <rameesraja27082002@gmail.com>
2025-04-13 01:02:00 +0530

    Completed

--- src/Sections/MyProjects.jsx
+++ src/Sections/MyProjects.jsx
@@ -2,10 +2,19 @@ import React from 'react'
 import useEmblaCarousel from 'embla-carousel-react'
 import { useState, useEffect, useCallback } from 'react'
 import { IoIosArrowForward } from 'react-icons/io'
-import {PROJECTS} from "../Utils/data"
+
 import ProjectCard from '../Components/ProjectCard'
 
-
+import PROJECT_IMG_1 from "../assets/images/project-1.png"
+import PROJECT_IMG_2 from "../assets/images/project-2.png"
+import PROJECT_IMG_3 from "../assets/images/project-3.png"
+import PROJECT_IMG_4 from "../assets/images/project-4.png"
+import PROJECT_IMG_5 from "../assets/images/project-5.png"
+import PROJECT_IMG_6 from "../assets/images/project-6.png"
+import PROJECT_IMG_7 from "../assets/images/project-7.png"
+import PROJECT_IMG_8 from "../assets/images/project-8.png"
+import PROJECT_IMG_9 from "../assets/images/project-9.png"
+import PROJECT_IMG_10 from "../assets/images/project-10.png"
 
 const MyProjects = () => {
 
@@ -26,7 +35,89 @@ const MyProjects = () => {
         updateScrollButtons();
     }, [emblaApi, updateScrollButtons]);
 
+   const PROJECTS = [
+       {
+           id: 1,
+           title: "Full Stack Task Manager App Using MERN | Mongo DB, Express JS, React JS, Node JS",
+           image: PROJECT_IMG_1,
+           tags: ["React", "Express JS", "Mongo DB", "Node JS"],
+           webisite: "https://task-apps-client.vercel.app/",
+           github: "https://github.com/RameesRaja-M/Task-App",
+       },
+       {
+           id: 2,
+           title: "Full Stack Blog  App Using MERN | Mongo DB, Express JS, React JS, Node JS",
+           image: PROJECT_IMG_2,
+           tags: ["React", "Express JS", "Mongo DB", "Node JS"],
+           webisite: "https://blogwhiz-website-frontend.vercel.app/home",
+           github: "https://github.com/RameesRaja-M/blogwhiz-website",
+       },
+       {
+           id: 3,
+           title: "Full Stack Bulk Mailer App Using MERN | Mongo DB, Express JS, React JS, Node JS",
+           image: PROJECT_IMG_3,
+           tags: ["React", "Express JS", "Mongo DB", "Node JS"],
+           webisite: "https://mailwhiz-frontend.vercel.app/",
+           github: "https://github.com/RameesRaja-M/Mailwhiz-Bulk-Mailer",
+       },
+       {
+           id: 4,
+           title: "Full Stack List Added App Using MERN | Mongo DB, Express JS, React JS, Node JS",
+           image: PROJECT_IMG_4,
+           tags: ["React", "Express JS", "Mongo DB", "Node JS"],
+           webisite: "https://fruit-react.vercel.app/",
+           github: "https://github.com/RameesRaja-M/Fruit-Website-React",
+       },
+       {
+           id: 5,
+           title: " Weather App Website Using React | HTML, CSS, JavaScript, React JS,",
+           image: PROJECT_IMG_5,
+           tags: ["React", "JavaScript", "Tailwind CSS", "CSS", "HTML"],
+           webisite: "https://weather-app-react-blue-psi.vercel.app/",
+           github: "https://github.com/RameesRaja-M/Weather-App-React",
+       },
+       {
+           id: 6,
+           title: " Udemy Clone Website Using React | HTML, CSS, Tailwind CSS, JavaScript, React JS,",
+           image: PROJECT_IMG_6,
+           tags: ["React", "JavaScript", "Tailwind CSS", "CSS", "HTML"],
+           webisite: "https://udemy-clone-react-dusky.vercel.app/",
+           github: "https://github.com/RameesRaja-M/Udemy-Clone-React",
+       },
+       {
+           id: 7,
+           title: " Perfume Website Using React | HTML, CSS, Tailwind CSS, JavaScript, React JS,",
+           image: PROJECT_IMG_7,
+           tags: ["React", "JavaScript", "Tailwind CSS", "CSS", "HTML"],
+           webisite: "https://perfumy-react-rho-flame.vercel.app/",
+           github: "https://github.com/RameesRaja-M/Perfumy-React",
+       },
+       {
+           id: 8,
+           title: " Nostra E-Commerce Website Using JavaScript | HTML, CSS, Tailwind CSS, JavaScript,",
+           image: PROJECT_IMG_8,
+           tags: ["JavaScript", "Tailwind CSS", "CSS", "HTML"],
+           webisite: "https://rameesraja-m.github.io/Nostra-Website/",
+           github: "https://github.com/RameesRaja-M/Nostra-Website",
+       },
+       {
+           id: 9,
+           title: " Greenden Website Using JavaScript | HTML, CSS, Tailwind CSS, JavaScript,",
+           image: PROJECT_IMG_9,
+           tags: ["JavaScript", "Tailwind CSS", "CSS", "HTML"],
+           webisite: "https://rameesraja-m.github.io/Greenden/",
+           github: "https://github.com/RameesRaja-M/Greenden",
+       },
+       {
+           id: 10,
+           title: "  Trip Advisor Clone Website Using HTML & CSS | HTML, CSS",
+           image: PROJECT_IMG_10,
+           tags: ["CSS", "HTML"],
+           webisite: "https://rameesraja-m.github.io/Trip-Advisor-Clone/",
+           github: "https://github.com/RameesRaja-M/Trip-Advisor-Clone",
+       },
    
+   ]
 
     return (
         <section id='projects' className='bg-background mt-14 md:p-6'>
