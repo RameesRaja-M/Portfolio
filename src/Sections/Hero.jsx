8f46e2df67dca877af6913ddc7b5fc9b64810fd1
RameesRaja-M <rameesraja27082002@gmail.com>
2025-04-19 09:08:55 +0530

    Completed

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -31,7 +31,10 @@ const Hero = () => {
             Skilled in React, Node.js, Express, and MongoDB.
             I love solving real-world problems and writing clean, efficient code.</p>
           <div className='flex justify-center lg:justify-start gap-4 md:gap-8 mt-6'>
-            <button className='flex-1 md:flex-none action-btn-outline btn-scale-anim'><a href="https://github.com/RameesRaja-M" target='blank'>View My Work</a></button>
+            <button className='flex-1 md:flex-none action-btn-outline btn-scale-anim'><a
+             href={"https://github.com/RameesRaja-M"}
+             target="_blank"
+             rel="noopener noreferrer">View My Work</a></button>
             <a
               href={"https://drive.google.com/file/d/1DFAZ67bHNEP8dAwsLy8T10QOFizkOX_U/view?usp=drive_link"}
               target="_blank"
